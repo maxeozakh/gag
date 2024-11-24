d03c2ae04dc356dd8ce76b2790da82bd9953bc8f
Maxim Zakharov <maxeozakh@gmail.com>
2024-11-24 13:29:18 +0100

    types (enable MyPy extension or smth) and data validation yay

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
+# will use this for compatibility
+# but more convinient syntax is already released: a | b
 from typing import Union
 
 from fastapi import FastAPI
 
+# data validation, serialisation
+from pydantic import BaseModel
+
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 
 @app.get('/')
@@ -12,3 +22,7 @@ def read_root():
 @app.get('/items/{item_id}')
 def read_item(item_id: int, q: Union[str, None] = None):
     return {'item_id': item_id, 'q': q}
+
+@app.put('/items/{item_id}')
+def update_item(item_id: int, item: Item):
+    return {'item_name': item.name, 'item_id': item_id}
